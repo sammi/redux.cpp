397b480dcf84d5bf7440bf682eca21301f38220a
Sammi <songyunhui2008@gmail.com>
2020-02-11 23:16:29 -0800

    upgrade gtest lib.

--- ReduxTest/any.cpp
+++ ReduxTest/any.cpp
@@ -1,47 +1,48 @@
 #include "pch.h"
 
-using namespace redux;
+namespace redux {
 
-TEST(Any, DefultConstructor) {
-	EXPECT_FALSE(Any());
-}
+	TEST(Any, DefultConstructor) {
+		EXPECT_FALSE(Any());
+	}
 
-TEST(Any, CopySameTypeConstructor) {
-	auto a = Any();
-	auto b = Any(a);
-	EXPECT_FALSE(b);
-	b = Any(1);
-	EXPECT_TRUE(b);
-	EXPECT_EQ(1, b.as<int>());
-}
+	TEST(Any, CopySameTypeConstructor) {
+		auto a = Any();
+		auto b = Any(a);
+		EXPECT_FALSE(b);
+		b = Any(1);
+		EXPECT_TRUE(b);
+		EXPECT_EQ(1, b.as<int>());
+	}
 
-TEST(Any, CopyWithConvertConstructor) {
-	auto a = Any(1);
-	EXPECT_TRUE(a);
-	EXPECT_EQ(1, a.as<int>());
-}
+	TEST(Any, CopyWithConvertConstructor) {
+		auto a = Any(1);
+		EXPECT_TRUE(a);
+		EXPECT_EQ(1, a.as<int>());
+	}
 
-TEST(Any, MoveConstructor) {
-	auto a = Any(1);
-	auto b = Any(std::move(a));
-	EXPECT_EQ(1, b.as<int>());
-	EXPECT_TRUE(b);
-	EXPECT_FALSE(a);
-}
+	TEST(Any, MoveConstructor) {
+		auto a = Any(1);
+		auto b = Any(std::move(a));
+		EXPECT_EQ(1, b.as<int>());
+		EXPECT_TRUE(b);
+		EXPECT_FALSE(a);
+	}
 
-TEST(Any, MoveAssignment) {
-	Any a{ 1 };
-	Any b{ 2 };
-	b = std::move(a);
-	EXPECT_FALSE(a);
-	EXPECT_EQ(1, b.as<int>());
-}
+	TEST(Any, MoveAssignment) {
+		Any a{ 1 };
+		Any b{ 2 };
+		b = std::move(a);
+		EXPECT_FALSE(a);
+		EXPECT_EQ(1, b.as<int>());
+	}
 
-TEST(Any, CopyAssignment) {
-	Any a{ 1 };
-	Any b{ 2 };
-	b = a;
-	EXPECT_TRUE(a);
-	EXPECT_EQ(1, a.as<int>());
-	EXPECT_EQ(1, b.as<int>());
+	TEST(Any, CopyAssignment) {
+		Any a{ 1 };
+		Any b{ 2 };
+		b = a;
+		EXPECT_TRUE(a);
+		EXPECT_EQ(1, a.as<int>());
+		EXPECT_EQ(1, b.as<int>());
+	}
 }
