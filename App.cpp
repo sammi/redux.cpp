c362c5cedbe07e0f1e2938f796e7402a289240f9
Sammi <songyunhui2008@gmail.com>
2020-02-15 00:27:14 -0800

    Move some static code into App.h

--- App.cpp
+++ App.cpp
@@ -5,64 +5,12 @@
 
 int main() {
 
-	const auto Increment = [](const App::State& state, int payload) {
-		return App::State{ state.counter() + payload };
-	};
-
-	const auto Decrement = [](const App::State& state, int payload) {
-		return App::State{ state.counter() - payload };
-	};
-
-	std::unordered_map<App::ActionType, std::function<const App::State(const App::State&, int)>> funcMap = {
-		{App::ActionType::increment, Increment},
-		{App::ActionType::decrement, Decrement}
-	};
-
-	const redux::Reducer<App::State>& reducer = [funcMap](const App::State& state, const redux::Action<>& action) {
-
-		App::ActionType actionType = action.type().as<App::ActionType>();
-
-		int payload = action.payload().as<int>();
-
-		auto search = funcMap.find(actionType);
-
-		if (search != funcMap.end() )
-			return search->second(state, payload);
-		else
-			return state;
-	};
-
-	const redux::MiddlewareDispatchTransform<App::State>& loggingMiddleware = [](const redux::Middleware<App::State>& middleware) {
-		return [middleware](const redux::Dispatch& dispatch) {
-			return [dispatch, middleware](const redux::Action<> action) {
-				BOOST_LOG_TRIVIAL(info) << "log before dispath action type: " << App::toString(action.type().as<App::ActionType>()) << " state:" << middleware.getState()().toString() << std::endl;
-				const redux::Action<>& next = dispatch(action);
-				BOOST_LOG_TRIVIAL(info) << "log after dispatch action type: " << App::toString(next.type().as<App::ActionType>()) << " state:" << middleware.getState()().toString() << std::endl;
-				return next;
-			};
-		};
-	};
-
-	const redux::MiddlewareDispatchTransform<App::State>& thunkMiddleware = [](const redux::Middleware<App::State>& middleware) {
-		return [middleware](const redux::Dispatch& dispatch) {
-			return [dispatch, middleware](const redux::Action<>& action) {
-				App::ActionType type = action.type().as<App::ActionType>();
-				if (type == App::ActionType::thunk) {
-					App::ThunkPayload thunkPayload = action.payload().as<App::ThunkPayload>();
-					thunkPayload(dispatch, middleware.getState());
-					return action;
-				}
-				return dispatch(action);
-			};
-		};
-	};
-
 	redux::Store<App::State> store = redux::createStore<App::State>(
-		reducer,
+		App::reducer,
 		App::State{},
 		{
-			thunkMiddleware,
-			loggingMiddleware
+			App::thunkMiddleware,
+			App::loggingMiddleware
 		}
 	);
 
@@ -71,15 +19,14 @@ int main() {
 	store.dispatch(App::MyAction<App::ActionType, App::ThunkPayload> {
 		App::ActionType::thunk,
 		[](const redux::Dispatch& dispatch, const redux::GetState<App::State>& getState) {
-			BOOST_LOG_TRIVIAL(info) << " state 1 : " << getState().toString() << std::endl;
 			dispatch(App::MyAction<App::ActionType, int>{App::ActionType::increment, 100 });
-			BOOST_LOG_TRIVIAL(info) << " state 2 : " << getState().toString() << std::endl;
-			dispatch(App::MyAction<App::ActionType, int>{App::ActionType::decrement, 367 });
-			BOOST_LOG_TRIVIAL(info) << " state 3 : " << getState().toString() << std::endl;
+			dispatch(App::MyAction<App::ActionType, int>{App::ActionType::decrement, 80});
 		}
 	});
 
-	store.dispatch(App::MyAction<App::ActionType, int>{App::ActionType::increment, 1000});
+	store.dispatch(App::MyAction<App::ActionType, int>{App::ActionType::increment, 10});
+
+	BOOST_LOG_TRIVIAL(info) << "end state: " << store.state().toString() << std::endl;
 
 	return 0;
 }
--- App.h
+++ App.h
@@ -29,6 +29,33 @@ namespace App {
 		thunk
 	};
 
+	const auto increment = [](const State& state, int payload) {
+		return State{ state.counter() + payload };
+	};
+
+	const auto decrement = [](const State& state, int payload) {
+		return State{ state.counter() - payload };
+	};
+
+	const std::unordered_map<ActionType, std::function<const State(const State&, int)>> funcMap = {
+		{ActionType::increment, increment},
+		{ActionType::decrement, decrement}
+	};
+
+	const redux::Reducer<State>& reducer = [](const State& state, const redux::Action<>& action) {
+
+		ActionType actionType = action.type().as<ActionType>();
+
+		int payload = action.payload().as<int>();
+
+		auto search = funcMap.find(actionType);
+
+		if (search != funcMap.end())
+			return search->second(state, payload);
+		else
+			return state;
+	};
+
 	using ThunkPayload = std::function<void(const redux::Dispatch, const redux::GetState<State>)>;
 
 	const std::string toString(ActionType type) {
@@ -43,4 +70,30 @@ namespace App {
 			return "";
 		}
 	}
+
+	const redux::MiddlewareDispatchTransform<State>& loggingMiddleware = [](const redux::Middleware<State>& middleware) {
+		return [middleware](const redux::Dispatch& dispatch) {
+			return [dispatch, middleware](const redux::Action<> action) {
+				BOOST_LOG_TRIVIAL(info) << "before dispath action " << toString(action.type().as<ActionType>()) << " state:" << middleware.getState()().toString() << std::endl;
+				const redux::Action<>& next = dispatch(action);
+				BOOST_LOG_TRIVIAL(info) << "after dispatch action " << toString(next.type().as<ActionType>()) << " state:" << middleware.getState()().toString() << std::endl;
+				return next;
+			};
+		};
+	};
+
+	const redux::MiddlewareDispatchTransform<State>& thunkMiddleware = [](const redux::Middleware<State>& middleware) {
+		return [middleware](const redux::Dispatch& dispatch) {
+			return [dispatch, middleware](const redux::Action<>& action) {
+				ActionType type = action.type().as<ActionType>();
+				if (type == ActionType::thunk) {
+					ThunkPayload thunkPayload = action.payload().as<ThunkPayload>();
+					thunkPayload(dispatch, middleware.getState());
+					return action;
+				}
+				return dispatch(action);
+			};
+		};
+	};
+
 }
